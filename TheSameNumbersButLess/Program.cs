0628e22720a4ea64ab804c9d96805becf76b007d
Serg_Goncharov <s.goncharov@sh.dunice.net>
2024-08-19 17:44:04 +0300

    test calls for the method NumbersConverter

--- TheSameNumbersButLess/Program.cs
+++ TheSameNumbersButLess/Program.cs
@@ -5,7 +5,14 @@ public class Programm
 {
     static void Main(string[] args) 
     {
-        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(3322));
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(21));
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(531));
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(2071));
+
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(9));
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(111));
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(135));
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(1027));
 
     }
 }
