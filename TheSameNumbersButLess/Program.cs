a9d35a30716ac15f71a54801f5f47abe5a4d5400
Serg_Goncharov <s.goncharov@sh.dunice.net>
2024-08-19 17:40:58 +0300

    Added NumbersConverter Method

--- TheSameNumbersButLess/Program.cs
+++ TheSameNumbersButLess/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using TheSameNumbersButLess.ToLessNimbersConverter;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Programm 
+{
+    static void Main(string[] args) 
+    {
+        Console.WriteLine(ToLessNumbersCoverter.NumbersConverter(3322));
+
+    }
+}
---
+++ TheSameNumbersButLess/ToLessNimbersConverter/ToLessNumbersCoverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSameNumbersButLess.ToLessNimbersConverter
+{
+    public class ToLessNumbersCoverter
+    {
+        public static int NumbersConverter(int number)
+        {
+            int[] num = number.ToString().Select(x => x - '0').ToArray();
+            if (num.Length <= 1)
+            {
+                return -1;
+            }
+
+            int lastIndex = 1;
+            int secondLastIndex = 2;
+
+            for (int i = 0; i < num.Length; i++, lastIndex++, secondLastIndex++)
+            {
+
+
+                int lastDigit = num[num.Length - lastIndex];
+                int secondLastDigit = num[num.Length - secondLastIndex];
+
+                if (lastDigit < secondLastDigit)
+                {
+                    num[num.Length - lastIndex] = (char)secondLastDigit;
+                    num[num.Length - secondLastIndex] = (char)lastDigit;
+
+                    if (Convert.ToInt32(num[0]) == 0)
+                    {
+                        return -1;
+                    }
+
+                    return Convert.ToInt32(new string(num.ToList().Select(x => Convert.ToChar(x + '0')).ToArray()));
+                }
+
+                if (num.Length <= secondLastIndex)
+                {
+                    return -1;
+                }
+
+                secondLastDigit = Convert.ToInt32(num[num.Length - (secondLastIndex + 1)]);
+
+                if (lastDigit < secondLastDigit)
+                {
+                    num[num.Length - lastIndex] = secondLastDigit;
+                    num[num.Length - (secondLastIndex + 1)] = lastDigit;
+
+                    if (Convert.ToInt32(num[0]) == 0)
+                    {
+                        return -1;
+                    }
+
+                    return Convert.ToInt32(new string(num.ToList().Select(x => Convert.ToChar(x + '0')).ToArray()));
+                }
+            }
+
+            return -1;
+        }
+    }
+}
